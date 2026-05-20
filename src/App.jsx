fffc285b8f29b37492cad52b8095a9762b57db5d
ez-sourav <souravb2003@gmail.com>
2026-05-20 20:53:57 +0530

    add hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
+import Hero from './Components/Hero'
 
 const App = () => {
   return (
-    <div className='bg-[#0B0B0C] min-h-screen text-white'>
+    <div className='bg-[#0B0B0C] min-h-screen text-white overflow-x-hidden'>
       <Navbar/>
+      <Hero/>
     </div>
   )
 }
