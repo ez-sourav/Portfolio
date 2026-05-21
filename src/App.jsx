d666eeb9ddee5958de8124d72dc826d4c9611a6c
ez-sourav <souravb2003@gmail.com>
2026-05-21 12:49:08 +0530

    add animation when scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from './Components/Navbar'
 import Hero from './Components/Hero'
+import About from './Components/About';
 
 const App = () => {
+
+  useEffect(() => {
+    const reveals = document.querySelectorAll(".reveal");
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add("active");
+          }
+        });
+      },
+      {
+        threshold: 0.15,
+      }
+    );
+
+    reveals.forEach((el) => observer.observe(el));
+
+    return () => observer.disconnect();
+  }, []);
   return (
     <div className='bg-[#0B0B0C] min-h-screen text-white overflow-x-hidden'>
       <Navbar/>
       <Hero/>
+      <About/>
     </div>
   )
 }
