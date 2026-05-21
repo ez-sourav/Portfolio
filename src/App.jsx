91279743a5086278689dfad054e1989e1594b757
ez-sourav <souravb2003@gmail.com>
2026-05-21 14:53:39 +0530

    add skills section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import Navbar from './Components/Navbar'
 import Hero from './Components/Hero'
 import About from './Components/About';
+import Skill from './Components/Skill';
 
 const App = () => {
 
@@ -26,10 +27,11 @@ const App = () => {
     return () => observer.disconnect();
   }, []);
   return (
-    <div className='bg-[#0B0B0C] min-h-screen text-white overflow-x-hidden'>
-      <Navbar/>
-      <Hero/>
-      <About/>
+    <div className='min-h-screen bg-[#0B0B0C] text-white'>
+      <Navbar />
+      <Hero />
+      <About />
+      <Skill />
     </div>
   )
 }
