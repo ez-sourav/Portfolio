c2610eac62d7a472126276c0042c2af7fac61023
ez-sourav <souravb2003@gmail.com>
2026-05-22 20:35:54 +0530

    add a dot before current role

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -38,13 +38,21 @@ const Hero = () => {
                 <div className="order-1 md:order-2 flex justify-center  lg:justify-end reveal  delay-200">
                     <div className="relative">
                         <img
-                            src="/pic.jpeg"
+                            src="/image.jpeg"
                             alt="Sourav Biswas"
                             className="w-[260px] h-[320px] sm:w-[300px] sm:h-[370px] md:w-[340px] md:h-[410px] lg:w-[340px] lg:h-[420px] object-cover rounded-2xl border border-orange-500/20"
                         />
                         <div className="absolute -bottom-5 -left-5 sm:-bottom-6 sm:-left-6 bg-[#1A1A1A] border border-orange-500/20 px-4 sm:px-6 py-3 sm:py-4 rounded-xl shadow-lg">
-                            <p className="text-xs sm:text-sm text-gray-400 mb-1">CURRENT ROLE</p>
-                            <h3 className="text-base sm:text-lg font-semibold">BCA Student & Developer</h3>
+                            <div className="flex items-center gap-2 mb-1">
+                                <span className="w-2 h-2 bg-orange-500 rounded-full shadow-[0_0_10px_#f97316] animate-pulse"></span>
+                                <p className="text-xs sm:text-sm text-gray-400 uppercase tracking-wider">
+                                    CURRENT ROLE
+                                </p>
+                            </div>
+
+                            <h3 className="text-base sm:text-lg font-semibold">
+                                BCA Student & Developer
+                            </h3>
                         </div>
                     </div>
                 </div>
